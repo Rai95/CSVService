f90465ad1d712cc5df9a2c2bd090033c9db8ca88
Rai95 <dream13276785298@163.com>
2019-01-09 00:02:23 +0800

    Initial commit

---
+++ CSVTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVService
+{
+    class CSVTest
+    {
+        public static void ReadCsv(string path)
+        {
+            DataTable dt = new DataTable();
+            
+
+        }
+
+
+    }
+}
