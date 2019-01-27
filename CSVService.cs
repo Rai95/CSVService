fdb5c0c12df081eee6098e751f4a9c60b979f20c
Rai95 <dream13276785298@163.com>
2019-01-27 11:05:45 +0800

    1

--- CSVService.cs
+++ CSVService.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSVService
 {
-    class CSVTest
+    class CSVService
     {
-        public static void ReadCsv(string path)
-        {
-            
-        }
-
-
     }
 }
